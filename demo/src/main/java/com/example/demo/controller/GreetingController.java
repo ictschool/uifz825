b90bfc8f87dfc2fb8179cfedf57d9ad2e0782856
buche2 <buchs.enrico@gmail.com>
2019-05-11 15:31:24 +0200

    path variable

--- demo/src/main/java/com/example/demo/controller/GreetingController.java
+++ demo/src/main/java/com/example/demo/controller/GreetingController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.websocket.server.PathParam;
 
 @Controller
 public class GreetingController {
@@ -18,6 +21,13 @@ public class GreetingController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         greetingService.saveGreeting(new Greeting(name));
+        model.addAttribute("greetings", greetingService.getGreetings());
+        return "test/greeting";
+    }
+
+    @GetMapping(value = "/delete/{id}/{name}")
+    public String deleteGreeting(@PathVariable("id") Integer id, Model model) {
+        greetingService.deleteGreeting(id);
         model.addAttribute("greetings", greetingService.getGreetings());
         return "test/greeting";
     }
